e281164355e52929bf4de70ba228fc8ac30f804c
Khoirul-Anam21 <khairulanamstud@gmail.com>
2023-01-16 22:01:33 +0700

    trying on Intro UI

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -64,7 +64,9 @@ class IdentityView extends StatelessWidget {
       padding: const EdgeInsets.only(right: 40, left: 0, top: 40, bottom: 40),
       child: Container(
           decoration: BoxDecoration(
-              border: isDark ? Border.all(color: Colors.amber.withOpacity(0.2)) : null,
+              border: isDark
+                  ? Border.all(color: Colors.amber.withOpacity(0.2))
+                  : null,
               boxShadow: isDark
                   ? null
                   : <BoxShadow>[
@@ -81,28 +83,93 @@ class IdentityView extends StatelessWidget {
               borderRadius: const BorderRadius.only(
                   topRight: Radius.circular(40),
                   bottomRight: Radius.circular(40))),
-          child: const Center(child: Text("TES"))),
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                PhotoIntroWidget(),
+                VerticalDivider(),
+                TextIntroWidget()
+              ],
+            ),
+          )),
     );
   }
 }
 
-// class AboutView extends StatelessWidget {
-//   const AboutView({
-//     Key? key,
-//   }) : super(key: key);
+class PhotoIntroWidget extends StatelessWidget {
+  const PhotoIntroWidget({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     bool isDark = context.watch<DarkThemeProvider>().darkTheme;
-//     print(isDark);
-//     return Container(
-//       color: Color.fromARGB(0, 125, 130, 190),
-//       // child: Row(
-//       //   children: const [
-//       //     Expanded(child: IntroTitle()),
-//       //     Expanded(child: IntroImage()),
-//       //   ],
-//       // ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const FlutterLogo(
+              size: 300,
+            ),
+            Text(
+              "Hi, I'm Anam",
+              style: Theme.of(context).textTheme.headline3,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextIntroWidget extends StatelessWidget {
+  const TextIntroWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 4,
+      child: Container(
+        padding: const EdgeInsets.only(left: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "I'm Mohammad Khoirul Anam",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Backend & Mobile App Developer",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline3!
+                  .copyWith(fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.resolveWith((states) =>
+                        const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(40))))),
+                onPressed: () {
+                  context.read<NavigationStateProvider>().changePageTo = 1;
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(14.0),
+                  child: Text("Let's Go"),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
